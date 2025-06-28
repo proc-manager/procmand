e926af0794a176707ad83cc16ace97d6a2a7a835
rutu-sh <ruturajs10@hotmail.com>
2025-06-28 18:26:19 -0400

    feat: add parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 mod process;
 mod common;
 
+use log::{info, LevelFilter};
+use env_logger::Builder;
 use process::parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    Builder::from_default_env()
+        .filter_level(LevelFilter::Info)
+        .init();
+
+    info!("reading the json config");
+
     let config = parser::parse("process.json")?;
 
     println!("config: {:?}", config);
+
+    info!("done reading json config");
 
     Ok(())
 }
