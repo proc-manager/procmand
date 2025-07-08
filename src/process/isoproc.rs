90bda64a9e66ff26da1eddd98f6c680e2dc1bbef
rutu-sh <ruturajs10@hotmail.com>
2025-07-08 12:27:09 -0400

    feat: some fixes

--- src/process/isoproc.rs
+++ src/process/isoproc.rs
@@ -1,17 +1,1 @@
-use crate::common::models::ProcessConfig;
-use nix::unistd;
 
-pub fn isoproc(pcfg: ProcessConfig) {
-
-    println!("{:?}", pcfg);
-
-}
-
-
-pub fn prepare_mntns(pcfg: &ProcessConfig) -> Result<(), Err> {
-
-    if unistd::geteuid() != 0 {
-
-    }
-
-}
