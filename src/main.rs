20b170f1d68c42af0be94ec228fbb74ec412afa0
rutu-sh <ruturajs10@hotmail.com>
2025-07-08 15:24:53 -0400

    feat: add piped ipc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod process;
 mod common;
 
+use std::io::{Read, Write};
+
 use env_logger::Builder;
 use log::{info, LevelFilter};
 
 use fork::{fork, Fork};
 use process::parser;
+use interprocess::os::unix as ipc_unix;
+use nix::unistd;
 
 use common::models::ProcessConfig;
 use process::isoproc;
@@ -24,12 +28,24 @@ use process::isoproc;
 */
 #[allow(dead_code)]
 fn start_process(pcfg: ProcessConfig) {
+    
+    let (mut p_send, mut c_recv) = ipc_unix::unnamed_pipe::pipe(true)
+        .expect("error creating p->c pipe");
+    let (mut c_send, mut p_recv) = ipc_unix::unnamed_pipe::pipe(true)
+        .expect("error creating c->p pipe");
+
     match fork() {
         Ok(Fork::Parent(child)) => {
             println!("continuing in parent process: {}", child);
+            let mut buf = [0; 2];
+            p_recv.read_exact(&mut buf).expect("parent: error reading");
+            p_send.write_all(String::from("OK").as_bytes()).expect("parent: error writing");
         },
         Ok(Fork::Child) => {
-            isoproc::setup_isoproc(&pcfg); 
+            info!("in child process");
+            isoproc::setup_isoproc(&pcfg, &mut c_recv, &mut c_send); 
+            unistd::close(p_recv).expect("unable to close p_recv");
+            unistd::close(p_send).expect("unable to close p_send");
         },
         Err(_) => {
             println!("fork failed");
--- src/process/isoproc.rs
+++ src/process/isoproc.rs
@@ -1,18 +1,28 @@
 use crate::common::models::ProcessConfig;
 
-use std::path;
+use std::{io::{Read, Write}, path};
 
 use log::info;
 use nix::{sched::{self, CloneFlags}, unistd};
+use interprocess::unnamed_pipe::{Sender, Recver};
 
-pub fn setup_isoproc(pcfg: &ProcessConfig) {
+
+pub fn setup_isoproc(pcfg: &ProcessConfig, recv: &mut Recver, sndr: &mut Sender) {
+
     
     info!("setting up the isolated process");
 
+    sndr.write_all(String::from("OK").as_bytes()).expect("error writing");
+    let mut buf = [0; 2];
+    recv.read_exact(&mut buf[..]).expect("error reading");
+
+
     unistd::chdir(path::Path::new(&pcfg.context_dir)).expect("unable to chdir");
     let cf = CloneFlags::CLONE_NEWNS;
-    sched::unshare(cf).expect("unable to unshare");
+    sched::unshare(cf).expect("cannot unshare");
     unistd::chroot(path::Path::new(&pcfg.context_dir)).expect("unable to chroot");
+
     println!("hello from chroot process");    
 
 }
+
